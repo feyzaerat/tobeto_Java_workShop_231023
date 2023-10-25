1ca034b962abf5fca181d948d2d83d5584a4588a
feyzaerat <feyzakrnfl@gmail.com>
2023-10-25 05:21:28 +0300

    created setter & getter in Product.java

--- oop1/src/main/java/org/example/Product.java
+++ oop1/src/main/java/org/example/Product.java
@@ -1,10 +1,49 @@
 package org.example;
 
 public class Product {
+       private String name;
+       private double unitPrice;
+       private double discount;
+       private String imageUrl;
+       private int unitsInStock;
 
-        String name;
-        double unitPrice;
-        double discount;
-        String imageUrl;
-        int unitsInStock;
-    }
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public double getUnitPrice() {
+                return unitPrice;
+        }
+
+        public void setUnitPrice(double unitPrice) {
+                this.unitPrice = unitPrice;
+        }
+
+        public double getDiscount() {
+                return discount;
+        }
+
+        public void setDiscount(double discount) {
+                this.discount = discount;
+        }
+
+        public String getImageUrl() {
+                return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+                this.imageUrl = imageUrl;
+        }
+
+        public int getUnitsInStock() {
+                return unitsInStock;
+        }
+
+        public void setUnitsInStock(int unitsInStock) {
+                this.unitsInStock = unitsInStock;
+        }
+}
