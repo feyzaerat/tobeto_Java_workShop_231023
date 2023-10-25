9c747d5205675af1695bcb67788e06b3f01e59e2
feyzaerat <feyzakrnfl@gmail.com>
2023-10-25 03:58:14 +0300

    created product1 in Product.java

--- oop1/src/main/java/org/example/Main.java
+++ oop1/src/main/java/org/example/Main.java
@@ -2,8 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String mesaj = "Vade Oradnı";
-        Product product1 = new Product();
-        product1.name = "Delonghi Kahve Makinesi";
+        String mesaj          = "Vade Oradnı";
+        Product product1      = new Product();
+        product1.name         = "Delonghi Kahve Makinesi";
+        product1.unitPrice    = 7500;
+        product1.discount     = 7;
+        product1.unitsInStock = 3;
+        product1.imageUrl     = "bilmemne.jpeg";
     }
 }
