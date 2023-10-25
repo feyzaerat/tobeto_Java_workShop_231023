fb7794d3630cc1578ef30e6b9bc9b8cb92220e4b
feyzaerat <feyzakrnfl@gmail.com>
2023-10-25 03:53:24 +0300

    created Main

--- oop1/src/main/java/org/example/Main.java
+++ oop1/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String mesaj = "Vade Oradnı";
+        Product product1 = new Product();
+        product1.name = "Delonghi Kahve Makinesi";
     }
 }
