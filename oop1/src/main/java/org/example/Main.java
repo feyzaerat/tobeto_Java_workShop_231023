8f8b9e714d99f9cd22b8dd12c2bb94ce8a3d348c
feyzaerat <feyzakrnfl@gmail.com>
2023-10-25 05:22:10 +0300

    created Customer[] in Main.java

--- oop1/src/main/java/org/example/Main.java
+++ oop1/src/main/java/org/example/Main.java
@@ -2,30 +2,51 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         String mesaj          = "Vade Oradnı";
 
         Product product1      = new Product();
-        product1.name         = "Delonghi Kahve Makinesi";
-        product1.unitPrice    = 7500;
-        product1.discount     = 7;
-        product1.unitsInStock = 3;
-        product1.imageUrl     = "unknown1.jpeg";
-
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setUnitPrice(7500);
+        product1.setUnitsInStock(7);
+        product1.setUnitsInStock(3);
+        product1.setImageUrl("unknown1.jpeg");
 
         Product product2      = new Product();
-        product2.name         = "Smeg Kahve Makinesi";
-        product2.unitPrice    = 6500;
-        product2.discount     = 8;
-        product2.unitsInStock = 4;
-        product2.imageUrl     = "unknown2.jpeg";
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setUnitPrice(6500);
+        product2.setDiscount(8);
+        product2.setUnitsInStock(4);
+        product2.setImageUrl("unknown2.jpeg");
 
         Product product3      = new Product();
-        product3.name         = "Kitchen Aid Kahve Makinesi";
-        product3.unitPrice    = 4500;
-        product3.discount     = 9;
-        product3.unitsInStock = 2;
-        product3.imageUrl     = "unknown3.jpeg";
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setUnitPrice(4500);
+        product3.setDiscount(9);
+        product3.setUnitsInStock(2);
+        product3.setImageUrl("unknown3.jpeg");
 
+        Product[] products = {product1,product2,product3};
+
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+
+        IndividualCustomer individualCustomer = new IndividualCustomer();
+        individualCustomer.setId(1);
+        individualCustomer.setPhone("05245682254");
+        individualCustomer.setCustomerNumber("125478");
+        individualCustomer.setFirstName("Feyza");
+        individualCustomer.setLastName("Erat");
+
+        CorporateCustomer corporateCustomer    = new CorporateCustomer();
+        corporateCustomer.setId(2);
+        corporateCustomer.setPhone("05246897541");
+        corporateCustomer.setCustomerNumber("258644");
+        corporateCustomer.setCompanyName("Avez Elektronik İletişim Eğitim Danışmanlığı Ticaret Anonim Şirketi");
+        corporateCustomer.setTaxNumber("2586463658");
+
+        Customer[] customers = {individualCustomer,corporateCustomer};
 
     }
 }
