bd2f3fc59fa82ebae13d167e95193cd051ca3561
feyzaerat <feyzakrnfl@gmail.com>
2023-10-25 06:16:35 +0300

    if loop for biggestNumber

--- recapDemo1/src/main/java/org/example/Main.java
+++ recapDemo1/src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+       int number1 = 20;
+       int number2 = 25;
+       int number3 = 2;
+
+       int biggestNumber = number1;
+
+       if(biggestNumber < number2){
+           biggestNumber = number2;
+       }
+       if(biggestNumber < number3){
+           biggestNumber = number3;
+       }
+
+       System.out.println("Biggest number is : " + biggestNumber);
 
     }
 }
