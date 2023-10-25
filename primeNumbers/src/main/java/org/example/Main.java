33e2e0dfb09b1062784f54dd71cbda11a52cfc46
feyzaerat <feyzakrnfl@gmail.com>
2023-10-25 06:59:54 +0300

    completed primeNumbers

--- primeNumbers/src/main/java/org/example/Main.java
+++ primeNumbers/src/main/java/org/example/Main.java
@@ -2,6 +2,34 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number    = 25;
+        int remainder = number % 2;
+        boolean isPrime = true;
+
+        if(number == 1){
+            System.out.println(number+"  is not a Prime Number");
+            return;
+        }
+        if(number < 1){
+            System.out.println(number+"  is an Invalid Number");
+            return;
+        }
+
+        System.out.println("________________________________________________________________");
+        System.out.println("The remainder of dividing "+number+" by 2 is " +remainder + " .");
+
+        for(int i = 2; i < number; i++){
+            if(number % i == 0){
+                isPrime = false;
+            }
+        }
+        System.out.println("________________________________________________________________");
+        if(isPrime){
+            System.out.println(number+"  is a Prime Number");
+        }
+        else{
+            System.out.println(number+"  is not a Prime Number");
+        }
+        System.out.println("________________________________________________________________");
     }
 }
