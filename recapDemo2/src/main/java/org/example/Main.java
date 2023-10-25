9c5c3da265a04a696f647e2d383225fe779336eb
feyzaerat <feyzakrnfl@gmail.com>
2023-10-25 06:31:59 +0300

    created array

--- recapDemo2/src/main/java/org/example/Main.java
+++ recapDemo2/src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       double[] myList = {1.2, 1.3, 4.3, 5.6};
+       double total = 0;
+       double max = myList[0];
+
+       for (double number:myList){
+           if(max < number){
+               max = number;
+           }
+
+           total = total + number;
+
+           System.out.println(number);
+           System.out.println("Sum : " + total);
+           System.out.println("*****************");
+           System.out.println("Biggest : " + max);
+       }
     }
 }
