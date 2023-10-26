b280efb803b81d994fbe74717c985ac10402f43b
feyzaerat <feyzakrnfl@gmail.com>
2023-10-26 16:25:42 +0300

    completed

--- findNumber/src/main/java/org/example/Main.java
+++ findNumber/src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int[] numbers = new int[]{1,2,5,7,9,0};
+       int willBeSearch = 5;
+       boolean isThere = false;
+
+       for (int number : numbers ){
+           if(number == willBeSearch){
+               isThere = true;
+               break;
+           }
+       }
+       if(isThere){
+           System.out.println("The number "+ willBeSearch + " you are looking for is available in this series." );
+       }else {
+
+           System.out.println("The number "+ willBeSearch + " you are looking for is not available in this series." );
+       }
     }
 }
