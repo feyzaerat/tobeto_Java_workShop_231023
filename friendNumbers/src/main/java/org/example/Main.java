61be5bf98e2b2c68bd91ca09a4e1b6db07ebaa66
feyzaerat <feyzakrnfl@gmail.com>
2023-10-26 16:10:51 +0300

    completed work

--- friendNumbers/src/main/java/org/example/Main.java
+++ friendNumbers/src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //220-284
+        int number1 = 220;
+        int number2 = 284;
+        int sum1 = 0;
+        int sum2 =0;
+
+        for (int i = 1; i < number1; i++){
+            if(number1 % i == 0){
+                sum1 = sum1 +i;
+            }
+        }
+
+        for (int i = 1; i<number2; i++){
+            if(number2 % i == 0){
+                sum2 = sum2 +i;
+            }
+        }
+
+        if(number1 == sum2 && number2 == sum1){
+            System.out.println("These two numbers are friend numbers.");
+        }else {
+            System.out.println("These two numbers are not friend numbers.");
+        }
     }
 }
