2a896609ceeecb13e8a19c4c81095feffcf57470
feyzaerat <feyzakrnfl@gmail.com>
2023-10-27 00:06:54 +0300

    getter&setter and constructors

--- fieLdandAttr/src/main/java/org/example/Main.java
+++ fieLdandAttr/src/main/java/org/example/Main.java
@@ -2,13 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product();
-        product.name = "Laptop";
-        product.id = 1;
-        product.description = "Asus";
-        product.price = 5000;
+        Product product = new Product(1,"Laptop", "Asus", 5000, 3);
+
+
+        /*****
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescription("Asus");
+        product.setPrice(5000);
         product.stockAmount = 3;
         System.out.println(product.name);
+         **********/
 
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
--- fieLdandAttr/src/main/java/org/example/Product.java
+++ fieLdandAttr/src/main/java/org/example/Product.java
@@ -1,9 +1,61 @@
 package org.example;
 
 public class Product {
+    public Product(int id, String name, String description, double price, int stockAmount){
+        System.out.println("Constructor method worked..");
+        this.id           = id;
+        this.name         = name;
+        this.description  = description;
+        this.price        = price;
+        this.stockAmount  = stockAmount;
+    }
+    public Product(){
+
+    }
     int id;
     String name;
     String description;
     double price;
     int stockAmount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.description = description;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
+
 }
